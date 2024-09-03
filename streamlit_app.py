8f6fdbaa88312dce2eaf4e48d7ce3cb73f0f9c86
Vismay Vilas Chaudhari <127238177+veeoid@users.noreply.github.com>
2024-09-03 11:19:54 -0500

    Update streamlit_app.py
    Updated fruityvice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,5 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon = "✅")
     
-fruityvice = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
