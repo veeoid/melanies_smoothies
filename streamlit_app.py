65caad280d52a2b33c147c20730aea4977d7708b
Vismay Vilas Chaudhari <127238177+veeoid@users.noreply.github.com>
2024-09-03 11:20:58 -0500

    Update streamlit_app.py
    Converted response to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon = "✅")
     
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
