7a8788010f68371204e8bd85aff302a79487df6a
Vismay Vilas Chaudhari <127238177+veeoid@users.noreply.github.com>
2024-09-03 11:16:10 -0500

    Update streamlit_app.py
    Added fruityvice api requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 
@@ -48,3 +49,5 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon = "✅")
     
+fruityvice = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
